aefcdf29be911e0cd86b586e4fe8a306fea17bd9
unknown <truongthuongvan203@gmail.com>
2025-03-25 09:45:06 +0700

    add checking command

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
+echo "Clearing cache..."
+php artisan config:clear
 
 echo "Caching config..."
 php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
+
+echo "Checking if postgres is running..."
+sudo systemctl status postgresql
 
 echo "Running migrations..."
 php artisan migrate --force
